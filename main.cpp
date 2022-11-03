321ab148189354a9f221c1462c7f855c0b799553
suminskiwiktor <245933@edu.p.lodz.pl>
2022-11-03 15:24:08 +0100

    dodano policz 2

--- main.cpp
+++ main.cpp
@@ -4,9 +4,13 @@ using namespace std;
 int policz(int a, int b){
     return a*b;
 }
+int policz2(int a,int b){
+    return a+b;
+}
 int main() {
     cout << "Hello, World!" << endl;
     cout<< "W pierwszym branchu 2*3="<<policz(2,3)<<endl;
+    cout<<"W drugim branchu 4+5="<<policz2(4,5)<<endl;
     return 0;
 }
 
