999c301b148eb83ab4b5597eb33bbcee40148414
suminskiwiktor <245933@edu.p.lodz.pl>
2022-11-03 14:55:48 +0100

    dodano linijke 10

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int policz(int a, int b){
 int main() {
     cout << "Hello, World!" << endl;
     cout<< "W pierwszym branchu 2+3="<<policz(2,3)<<endl;
-    cout<<"test branchy"<<endl;
+    cout<<"test branchy "<<endl;
     return 0;
 }
 
